345dd322a255a282ca32efbedec274bc6abd982e
vslayer34 <mohamd.elnagar.34@gmail.com>
2025-04-05 09:37:41 +0200

    Added Asteroid spawning in the game manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -5,6 +6,11 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set;}
 
     public ObjectPool ExplosionPool { get; private set; }
+
+    [SerializeField]
+    private ObjectPool[] _asteroidsPools = new ObjectPool[3];
+
+    private bool _isGameRunning = true;
 
 
 
@@ -19,5 +25,29 @@ public class GameManager : MonoBehaviour
         }
 
         Instance = this;
+    }
+
+    private IEnumerator Start()
+    {
+        // start Game
+
+        // Game Loop
+        while (_isGameRunning)
+        {
+            SpawnAsteroid();
+            yield return new WaitForSeconds(Random.Range(1, 5));
+        }
+
+        // End Game
+    }
+
+    // Member Methods------------------------------------------------------------------------------
+
+    private void SpawnAsteroid()
+    {
+        int i = Random.Range(0, _asteroidsPools.Length);
+
+        var newAsteroid = _asteroidsPools[i].GetPoolObject();
+        newAsteroid.SetActive(true);
     }
 }
